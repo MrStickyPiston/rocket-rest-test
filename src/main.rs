cc39543ffc53c38762dfcbd9b578282867ec5ae6
sticky <mr.sticky.piston@gmail.com>
2025-04-29 17:27:46 +0200

    convert to state

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@
 
 use rocket::serde::{Serialize, Deserialize, json::Json};
 use rocket::serde::json::serde_json;
-use rocket_db_pools::{Database};
+use rocket::State;
 
-use rocket_db_pools::Connection;
 use sqlx;
 
 #[derive(Serialize, Debug)]
@@ -28,34 +27,42 @@ fn index() -> Json<Response<String>> {
     Json(Response{value: "test".to_string()})
 }
 
-#[derive(Database)]
-#[database("db")]
-struct Db(sqlx::SqlitePool);
-
 #[get("/")]
-async fn get_roots(mut db: Connection<Db>) -> Option<Json<Response<Vec<NounRootsTable>>>> {
+async fn get_roots(state: &State<AppState>) -> Option<Json<Response<Vec<NounRootsTable>>>> {
     sqlx::query_as("SELECT * FROM noun_roots_table")
-        .fetch_all(&mut **db).await
+        .fetch_all(&state.pool).await
 	.map(|v| {Response{value: v}})
         .map(Json)
         .ok()
 }
 
 #[get("/<id>")]
-async fn get_root(mut db: Connection<Db>, id: i64) -> Option<Json<Response<NounRootsTable>>> {
+async fn get_root(state: &State<AppState>, id: i64) -> Option<Json<Response<NounRootsTable>>> {
     sqlx::query_as("SELECT * FROM noun_roots_table WHERE id = ?").bind(id)
-        .fetch_one(&mut **db).await
+        .fetch_one(&state.pool).await
 	.map(|v| {Response{value: v}})
 	.map(Json)
 	.ok()
 }
 
+struct AppState {
+	pool: sqlx::PgPool
+}
+
 #[shuttle_runtime::main]
-async fn rocket(#[shuttle_shared_db::Postgres] pool: sqlx::PgPool) -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build()
-	.mount("/", routes![index])
-	.mount("/roots/", routes![get_roots, get_root])
-	.attach(Db::init());
-	    Ok(rocket.into())
+async fn rocket(
+	#[shuttle_shared_db::Postgres(
+		local_uri = "postgresql://user@127.0.0.1:5432/rest-test"
+	)]
+	pool: sqlx::PgPool
+) -> shuttle_rocket::ShuttleRocket {
+	let state = AppState { pool };
+ 
+	let rocket = rocket::build()
+		.mount("/", routes![index])
+		.mount("/roots/", routes![get_roots, get_root])
+		.manage(state);
+	
+	Ok(rocket.into())
 }
 
